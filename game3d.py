cb0dbf987a590f655186509427b92346aac98e5f
aimetz <67288550+aimetz@users.noreply.github.com>
2020-12-15 02:37:07 -0800

    Add files via upload

--- game3d.py
+++ game3d.py
@@ -23,9 +23,11 @@ def main():
     b = (0, 0, 255)
     g = (0, 255, 0)
     red = (235, 0, 0)
-    trans = np.dot(np.asmatrix(np.array([[1, 0, 0], [0, 1, 0]])), rotate_x(7))
-    corns = [np.asmatrix(np.array([[0], [5000], [0]])), np.asmatrix(np.array([[5000], [5000], [0]])), 
-             np.asmatrix(np.array([[5000], [0], [0]])), np.asmatrix(np.array([[0], [0], [0]]))]
+    x1 = -2500
+    x2 = 2500
+    y1 = -2500
+    y2 = 2500
+    trans = np.dot(np.asmatrix(np.array([[1, 0, 0], [0, 1, 0]])), rotate_x(6.25))
     #corns3 = [np.asmatrix(np.array([[0], [500], [0]])), np.asmatrix(np.array([[500], [500], [0]])), np.asmatrix(np.array([[500], [0], [0]])),
     #          np.asmatrix(np.array([[500], [0], [-2]])), np.asmatrix(np.array([[500], [500], [-10]])), np.asmatrix(np.array([[0], [500], [-8]]))]
     print("Use arrows to move around and z/x to look around")
@@ -40,47 +42,40 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     pygame.key.set_repeat(5)
-                    man.step([[2], [0], [0]])
+                    x1 += 2
+                    x2 += 2
                 if event.key == pygame.K_RIGHT:
                     pygame.key.set_repeat(5)
-                    man.step([[-1], [0], [0]])
+                    x1 -= 2
+                    x2 -= 2
                 if event.key == pygame.K_DOWN:
                     pygame.key.set_repeat(5)
-                    man.step([[0], [-2], [0]])
+                    y1 += 2
+                    y2 += 2
                 if event.key == pygame.K_UP:
-                   pygame.key.set_repeat(5)
-                   man.step([[0], [2], [0]])
-                if event.key == pygame.K_SPACE:
-                    pygame.key.set_repeat()
-                    if man.pos.item(2) == -100:
-                        man.step([[0], [0], [100]])
-                    else:
-                        man.step([[0], [0], [-100]])
-                if event.key == pygame.K_a:
-                    pygame.key.set_repeat()
-                    rotations[0] -= 1
-                    trans = np.dot(trans, rotate_x(-1))
-                if event.key == pygame.K_d:
-                    pygame.key.set_repeat()
-                    rotations[0] += 1
-                    trans = np.dot(trans, rotate_x(1))
-                if event.key == pygame.K_s:
-                    pygame.key.set_repeat()
-                    rotations[1] -= 1
-                    trans = np.dot(trans, rotate_y(-1))
-                if event.key == pygame.K_w:
-                    pygame.key.set_repeat()
-                    rotations[1] += 1
-                    trans = np.dot(trans, rotate_y(1))
+                    pygame.key.set_repeat(5)
+                    y1 -= 2
+                    y2 -= 2
                 if event.key == pygame.K_z:
                     pygame.key.set_repeat()
                     rotations[2] -= 1
+                    trans = np.dot(trans, rotate_x(.25))
                     trans = np.dot(trans, rotate_z(-1))
+                    trans = np.dot(trans, rotate_x(-.25))
                 if event.key == pygame.K_x:
                     pygame.key.set_repeat()
                     rotations[2] += 1
+                    trans = np.dot(trans, rotate_x(.25))
                     trans = np.dot(trans, rotate_z(1))
-        mp = np.dot(trans, man.pos)
+                    trans = np.dot(trans, rotate_x(-.25))
+                if event.key == pygame.K_a:
+                    pygame.key.set_repeat()
+                    trans = np.dot(trans, rotate_x(.5))
+                if event.key == pygame.K_d:
+                    pygame.key.set_repeat()
+                    trans = np.dot(trans, rotate_x(-.5))
+        corns = [np.asmatrix(np.array([[x1], [y2], [0]])), np.asmatrix(np.array([[x2], [y2], [0]])), 
+                 np.asmatrix(np.array([[x2], [y1], [0]])), np.asmatrix(np.array([[x1], [y1], [0]]))]
         po = []
         #po2 = []
         #po3 = []
@@ -97,14 +92,15 @@ def main():
         pygame.draw.polygon(screen, g, po)
 
         lines = []
-        for q in range(0, 5000, 100):
-            a = np.dot(trans, np.asmatrix(np.array([[q], [0], [0]])))
-            aa = np.dot(trans, np.asmatrix(np.array([[q], [5000], [0]]))) 
-            b = np.dot(trans, np.asmatrix(np.array([[0], [q], [0]])))
-            bb = np.dot(trans, np.asmatrix(np.array([[5000], [q], [0]])))
+        for q in range(x1, x2, 100):
+            a = np.dot(trans, np.asmatrix(np.array([[q], [y1], [0]])))
+            aa = np.dot(trans, np.asmatrix(np.array([[q], [y2], [0]]))) 
             lines += [((a.item(0)+man.pos.item(0), a.item(1)+man.pos.item(1)), 
-                      (aa.item(0)+man.pos.item(0), aa.item(1)+man.pos.item(1))), 
-                      ((b.item(0)+man.pos.item(0), b.item(1)+man.pos.item(1)), 
+                      (aa.item(0)+man.pos.item(0), aa.item(1)+man.pos.item(1)))]
+        for q in range(y1, y2, 100):
+            b = np.dot(trans, np.asmatrix(np.array([[x1], [q], [0]])))
+            bb = np.dot(trans, np.asmatrix(np.array([[x2], [q], [0]])))
+            lines += [((b.item(0)+man.pos.item(0), b.item(1)+man.pos.item(1)), 
                       (bb.item(0)+man.pos.item(0), bb.item(1)+man.pos.item(1)))]
         for line in lines:
             pygame.draw.line(screen, red, line[0], line[1], 1)
